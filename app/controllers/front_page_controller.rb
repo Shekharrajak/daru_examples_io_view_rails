a3983b6aedadad427841f8363c054ffc767c5a3a
shekharrajak <shekharstudy@ymail.com>
2017-08-02 23:47:35 +0530

    home page added . working fine with data_tables and links added

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -4,10 +4,19 @@ class FrontPageController < ApplicationController
 
   def home
     @df_links = Daru::DataFrame.new({
-      routes:         ['Repo forks and issue count'],
-      Desc:           ['Analysis of fork and issue count present in sciruby org'],
-      Localhost_link: ['http://localhost:3000/sciruby/repos']
-      })
-    @df_links_table = Daru::View::Table.new(@df_links, pageLength: 3, adapter: :datatables)
+      routes:   [
+        'Repo forks and issue count',
+        'Popular repo of SciRuby'
+        ],
+      Desc:     [
+        'Analysis of fork and issue count present in sciruby org',
+        'Popular SciRuby repo in the point of view of number of watchers'
+        ],
+      Localhost_link: [
+        'http://localhost:3000/sciruby/repos',
+        'http://localhost:3000/sciruby/watchers'
+        ]
+      }, order: [:routes, :Desc, :Localhost_link])
+    @df_links_table = Daru::View::Table.new(@df_links, pageLength: 3, adapter: :datatables, height: 500, width: 700)
   end
 end
