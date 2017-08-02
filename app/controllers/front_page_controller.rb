368f94ac11da004325cf7035ec9539cc6e3932f9
shekharrajak <shekharstudy@ymail.com>
2017-08-02 23:36:47 +0530

    home page data_tables error fixed added

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -1,12 +1,13 @@
 class FrontPageController < ApplicationController
 
-  layout :datatables_layout
+  layout 'datatables_layout'
+
   def home
     @df_links = Daru::DataFrame.new({
       routes:         ['Repo forks and issue count'],
       Desc:           ['Analysis of fork and issue count present in sciruby org'],
       Localhost_link: ['http://localhost:3000/sciruby/repos']
       })
-    @df_links_table = Daru::View::Table.new(df2, pageLength: 3, adapter: :datatables)
+    @df_links_table = Daru::View::Table.new(@df_links, pageLength: 3, adapter: :datatables)
   end
 end
