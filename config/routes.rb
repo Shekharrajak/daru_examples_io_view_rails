3673389de875d0d8c20f37d658d2d13f4a99aa74
shekharrajak <shekharstudy@ymail.com>
2017-08-02 22:10:02 +0530

    home page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/sciruby/repos', to: 'sciruby_github#repo'
+  get '/sciruby/watchers', to: 'sciruby_github#watchers'
 end
