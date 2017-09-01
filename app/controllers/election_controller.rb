7890bc1f34cfecfb8d9a1aad775d049e016797f3
zverok <zverok.offline@gmail.com>
2017-09-01 18:06:20 +0300

    Cleanup election code

--- app/controllers/election_controller.rb
+++ app/controllers/election_controller.rb
@@ -3,22 +3,10 @@ class ElectionController < ApplicationController
   layout :resolve_layout
 
   def data
-    @df_stock = Daru::DataFrame.from_html('http://eciresults.nic.in/PartyWiseResult.htm').first
-    @df_stock.delete_row(-1)
-    data = @df_stock.map_vectors { |x| [x.name, x.to_a.map { |y| y.to_i.to_s==y ? y.to_i : y } ] }.to_h
-    new_df = Daru::DataFrame.new('Party' => data['Party'], 'Won' => data['Won'])
-    opts = {
-      adapter: :googlecharts,
-      height: 500,
-      width: 1000
-    }
-    table_opts = {
-      adapter: :googlecharts, pageSize: 10,
-      height: 300, width: 400
-    }
-    @df_stock_table     = Daru::View::Table.new(@df_stock, table_opts)
-    @df_stock_pie_chart = Daru::View::Plot.new(new_df, type: :pie, **opts)
-    @df_stock_col_chart = Daru::View::Plot.new(new_df, type: :histogram, **opts)
+    @elections = Daru::DataFrame
+      .from_html('http://eciresults.nic.in/PartyWiseResult.htm').first
+      .tap { |df| df.delete_row(-1) }
+      .recode_vectors { |v| v.name == 'Party' ? v : v.recode(&:to_i) }
   end
 
   private
